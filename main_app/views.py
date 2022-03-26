e3338d5e70efe04f80e995396e31f9c2883d98ff
joewhitneywalker <walker.joe81@gmail.com>
2022-03-26 16:53:12 -0700

    updating user profile auth

--- main_app/views.py
+++ main_app/views.py
@@ -1,8 +1,12 @@
 from ast import Delete
 from sre_constants import SUCCESS
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import render
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 from django.urls import reverse
 from django.views.generic.base import TemplateView
 from django.views import View
@@ -66,6 +70,7 @@ class Character_List(TemplateView):
         return context
 
 #create a new character
+@method_decorator(login_required, name='dispatch')
 class Character_Create(CreateView):
   model = Character
   fields = ['name', 'img', 'age', 'attribute', 'gender', 'characterpowers']
@@ -87,6 +92,7 @@ class Character_Detail(DetailView):
     template_name = "character_detail.html"
 
 #update view
+@method_decorator(login_required, name='dispatch')
 class Character_Update(UpdateView):
     model = Character
     fields = ['name', 'img', 'age', 'attribute', 'gender', 'characterpowers']
@@ -96,12 +102,14 @@ class Character_Update(UpdateView):
         return reverse('character_detail', kwargs={'pk': self.object.pk})
 
 #delete view
+@method_decorator(login_required, name='dispatch')
 class Character_Delete(DeleteView):
     model = Character
     template_name = "character_delete_confirmation.html"
     success_url = "/characters/"
 
 #user function
+@login_required
 def profile(request, username):
     user = User.objects.get(username=username)
     characters = Character.objects.filter(user=user)
@@ -117,21 +125,63 @@ def characterpowers_show(request, characterpowers_id):
     characterpowers = CharacterPowers.objects.get(id=characterpowers_id)
     return render(request, 'characterpowers_show.html', {'characterpowers': characterpowers})
 
-
+@method_decorator(login_required, name='dispatch')
 class CharacterPowersCreate(CreateView):
     model = CharacterPowers
     fields = '__all__'
     template_name = "characterpowers_form.html"
     success_url = '/characterpowers'
 
- 
+@method_decorator(login_required, name='dispatch')
 class CharacterPowersUpdate(UpdateView):
     model = CharacterPowers
-    fields = ['name', 'color']
+    fields = '__all__'
     template_name = "characterpowers_update.html"
     success_url = '/characterpowers'   
 
+@method_decorator(login_required, name='dispatch')
 class CharacterPowersDelete(DeleteView):
     model = CharacterPowers
     template_name = "characterpowers_confirm_delete.html"
     success_url = '/characterpowers'
+
+#django auth
+def signup_view(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            print('HEY', user.username)
+            return HttpResponseRedirect('/user/'+str(user))
+        else:
+            HttpResponse('<h1>Try Again</h1>')
+    else:
+        form = UserCreationForm()
+        return render(request, 'signup.html', {'form': form})
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect('/characters')
+
+def login_view(request):
+     # if post, then authenticate (user submitted username and password)
+    if request.method == 'POST':
+        form = AuthenticationForm(request, request.POST)
+        # form = LoginForm(request.POST)
+        if form.is_valid():
+            u = form.cleaned_data['username']
+            p = form.cleaned_data['password']
+            user = authenticate(username = u, password = p)
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return HttpResponseRedirect('/user/'+u)
+                else:
+                    print('The account has been disabled.')
+            else:
+                print('The username and/or password is incorrect.')
+    else: # it was a get request so send the emtpy login form
+        # form = LoginForm()
+        form = AuthenticationForm()
+        return render(request, 'login.html', {'form': form}) 
